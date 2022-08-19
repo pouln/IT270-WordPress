11bfc96e67886ab75d2f2f0124e69297158731f3
Poul <poulnichols@outlook.com>
2022-08-19 06:01:10 -0700

    update final

--- Final/footer.php
+++ Final/footer.php
@@ -14,8 +14,8 @@
 			</li>
 			<li>All Rights Reserved</li>
 			<li><a href="">Coding By Poul</a></li>
-			<li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fcodingbypoul.com%2Fit270%2Fsite1%2F"> VALID HTML</a></li>
-			<li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fcodingbypoul.com%2Fit270%2Fsite1%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">VALID CSS</a></li>	
+			<li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fcodingbypoul.com%2Fit270%2Ffinal%2F"> VALID HTML</a></li>
+			<li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fcodingbypoul.com%2Fit270%2Ffinal%2F&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en">VALID CSS</a></li>	
 		</ul>
 	</div><!-- end inner-footer -->
     
